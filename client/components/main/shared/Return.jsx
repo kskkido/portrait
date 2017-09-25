4a301a30748b9d576f8a9565be96c4e83f1125d4
Keisuke Kido <kskkido@gmail.com>
2017-09-26 01:39:01 +0900

    change to block

--- client/components/main/shared/Return.jsx
+++ client/components/main/shared/Return.jsx
@@ -25,7 +25,7 @@ const HomeTextContainer = styled.div`
   text-align: center;
   opacity: 0;
   scale: 0;
-  ${media.phone`opacity: 1; scale: 1; height: 50px;`}
+  ${media.phone`opacity: 1; scale: 1; display: block;`}
 `
 
 const Return = ({ isBody, hide, text, onClick, onHover, onHoverOff, inputButton, inputText }) => (
